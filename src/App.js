875a08b622c5df97b5209e9f45b75622ebe6c324
Kozlov Arseny <jetpilotat1il@gmail.com>
2024-07-12 19:01:42 +0300

    add header with navigation

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter } from 'react-router-dom';
+import Header from './components/Header/Header';
 
 function App() {
   return (
-    <div className="App">
-      Hello world!!!
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Header/>
+      </div>
+    </BrowserRouter>
   );
 }
 
