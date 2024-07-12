998e01c72a837995f5f62ad02c9efc74984a0352
Kozlov Arseny <jetpilotat1il@gmail.com>
2024-07-12 20:14:51 +0300

    add home icon in header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,13 @@
 import styles from './Header.module.css'
 import { NavLink } from 'react-router-dom'
-
+import home from '../../Assets/home.png'
 
 const Header = () => {
     return(
         <nav className={styles.header}>
             <div>
                 <NavLink to='/'>
-                    <img src='https://sun9-78.userapi.com/impg/a0T-l71_eLBlL0tDBsspEGc7efEbRHU6meFxyw/RhGIUCzThYs.jpg?size=200x267&quality=96&sign=88178f7e1940f7e2d487fcf6a80cf2c2&c_uniq_tag=Y4wSwc60xrSVq5y1HgNzrlCcWbHRcjulf_UHCFMn8vg&type=album'></img>
+                    <img src={home}></img>
                 </NavLink>
             </div>
             <div className={styles.item}>
